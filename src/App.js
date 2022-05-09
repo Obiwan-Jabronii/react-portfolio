2d2199cb9277379184c4ca78f2a1cc11d6472b03
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-05-09 10:56:58 -0400

    added a few comments and cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import About from "../src/components/About/index";
+import Nav from "../src/components/Nav/index";
+//import Gallery from "../src/components/Gallery/index";
+import "./index.css"
+
 
 function App() {
+  // creates the categories for the nav bar through prop drilling 
+  const [categories] = useState([
+    {
+      name: 'education'
+    },
+    {
+      name: 'experience'
+    },
+    {
+      name: 'projects'
+    },
+    {
+      name: 'contact'
+    }
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+  // render main page with the serperate components 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <About></About>
+      </main>
+
     </div>
   );
 }
---
+++ src/utils/helpers.js
@@ -0,0 +1,4 @@
+//capitalize  the first letter of all words in the string 
+export function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
