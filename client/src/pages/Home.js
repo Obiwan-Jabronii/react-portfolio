1709471f70c584360e0291a466e1a3979020bde9
Obi Wan Jabronii <mitchell_lough@yahoo.com>
2022-10-11 15:19:30 -0400

    updated projects

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,9 +4,9 @@ const Home = () => {
 
     return (
         <main>
-            <div className='flex-column justify-space-around home-container p-4' style={{ width: 600, border: "solid #049434 5px", }}>
+            <div className='flex-column justify-space-around home-container p-4' style={{  border: "solid #049434 5px", maxWidth: 600 }}>
                 <h1 className='text-light'style={{ fontSize: 60 }}>Welcome!</h1>
-                <p className='home-p'>Thank you for visiting my interactive portfolio! My name is Mitchell Lough and I have been an aspiring Full-Stack Web Developer since the spring of 2021. After receiving my certification through Michigan State University I began my journey.</p>
+                <p className='home-p text-shrink'>Thank you for visiting my interactive portfolio! My name is Mitchell Lough and I have been an aspiring Full-Stack Web Developer since the spring of 2021. After receiving my certification through Michigan State University I began my journey.</p>
             </div>
         </main>
     )
